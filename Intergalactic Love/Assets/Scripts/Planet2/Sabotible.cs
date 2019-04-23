523d2477a5c170f6f607c32996e12ae1a983f1dd
GabrielVidal1 <gvidalayrinhac@gmail.com>
2019-04-22 22:07:20 -0700

    Added planet 2

---
+++ Intergalactic Love/Assets/Scripts/Planet2/Sabotible.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sabotible : Interactible
+{
+    public GameObject vaisseau;
+
+
+    public override void Interact(Player player)
+    {
+        Destroy(vaisseau);
+    }
+}
---
+++ Intergalactic Love/Assets/mechantZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mechantZone : MonoBehaviour
+{
+    public Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetTrigger("Dead");
+
+
+            Invoke("couille", 3);
+        }
+    }
+
+    private void couille()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
